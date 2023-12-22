0c0313660699747a89b1f991d86ad5c9f5635dcc
jazimin1905 <154576557+jazimin1905@users.noreply.github.com>
2023-12-21 23:58:51 -0500

    added necessaery changes

--- client3.py
+++ client3.py
@@ -1,3 +1,4 @@
+
 ################################################################################
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a
@@ -35,25 +36,39 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
-
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
-
+    if price_b == 0:
+        return None  # Indicate that the ratio is undefined (or you can handle it differently as per your requirements)
+    return price_a / price_b
 
 # Main
 if __name__ == "__main__":
-    # Query the price once every N seconds.
+    prices = {}
+
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
         for quote in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            stock, price = getDataPoint(quote)
+            print("Quoted %s at price: %s" % (stock, price))
 
-        print("Ratio %s" % getRatio(price, price))
+            prices[stock] = price
+
+        stock_names = list(prices.keys())
+        for i in range(len(stock_names)):
+            for j in range(i + 1, len(stock_names)):
+                stock1 = stock_names[i]
+                stock2 = stock_names[j]
+                ratio = getRatio(prices[stock1], prices[stock2])
+
+                if ratio is not None:
+                    print("Ratio between %s and %s is %s" % (stock1, stock2, ratio))
+                else:
+                    print("Cannot compute ratio between %s and %s: price_b is zero" % (stock1, stock2))
+
+        prices.clear()
+
